83c6dd7107be4e333ee22ba6d0cdcbaf310c071e
sireeprojects <sireeprojects@gmail.com>
2018-04-22 18:51:45 +0530

    graphics project

---
+++ basics_qgraphics/top.cpp
@@ -0,0 +1,14 @@
+#include "top.h"
+#include "ui_top.h"
+
+top::top(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::top)
+{
+    ui->setupUi(this);
+}
+
+top::~top()
+{
+    delete ui;
+}
