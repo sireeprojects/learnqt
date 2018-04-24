98a50c1ddc6f7d8448a94edf3163a9cb49732a1a
sireeprojects <sireeprojects@gmail.com>
2018-04-25 00:02:54 +0530

    new examples

--- test_stylesheet/main.cpp
+++ test_stylesheet/main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    app.setStyle("Fusion");
+    app.setStyle("Windows");
 
     QFont myfont;
     myfont.setFamily("Segoe UI");
